6147780c4b36a0ec22221d12337f3f6f7714c861
Abraham Lincoln <abraham.lincoln@example.com>
2019-07-22 17:29:56 +0000

    Changes by 'Abraham Lincoln' on 'DemoTasker123 – 1.1.1' -- Build #1

---
+++ sn/DemoTasker123/Business Rule/x_dtll2_demotasker_new_poc_demo_table_1/active_true/before/New POC Business Rule 1/script.js
@@ -0,0 +1,27 @@
+/* 
+ * Application : DemoTasker123
+ * ClassName   : sys_script
+ * Created On  : 2019-06-28 20:12:34
+ * Created By  : admin
+ * Updated On  : 2019-07-16 20:13:34
+ * Updated By  : abraham.lincoln
+ * URL         : /sys_script.do?sys_id=dbd5f88edb1af300936dd498f49619cb
+ */
+/**
+ * Business rule for New POC Demo Table 1
+ * 
+ * @method executeRule
+ * @author POC Admin Svr [admin]
+ * @memberof x_dtll2_demotasker.module:sys_script
+ * @returns {undefined}
+ * @param current the current record
+ * @param previous the previous record
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	gs.info("Hello World!!");
+	gs.info("Hello World!!");
+gs.info("Hello World2!!");
+// 	gs.info("Hello Za Worldo!");
+
+})(current, previous);
